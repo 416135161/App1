7b38563ed371d924ad8732e827f28eaee2062f10
ningshijie <416135161@qq.com>
2017-06-30 09:40:53 +0800

    修改发送短信的方式

--- src/com/internet/tools/MessageSender.java
+++ src/com/internet/tools/MessageSender.java
@@ -65,10 +65,10 @@ public class MessageSender {
 
 	public void sendSms(String custNums, String msgContent, Context context,
 			boolean isCheck) {
-//		if (isCheck) {
-//			if (!UserSession.getCheckSendMsg(context))
-//				return;
-//		}
+		if (isCheck) {
+			if (!UserSession.getCheckSendMsg(context))
+				return;
+		}
 		new SendSmsThread(custNums, msgContent, null).start();
 //		Date date = new java.util.Date();
 //		msgContent = "*" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(date) + "*";
